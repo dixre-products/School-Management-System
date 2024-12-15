537dcbc7c6c13a5e0e1c4c5864f4a776da44f18f
Salihat Balarabe <balarabesally@gmail.com>
2024-12-15 11:09:40 +0100

    School Management System

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "School Management System",
+  description:
+    "School Management System dashboard to manage all activities in a school.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,28 @@
+import { menuItems } from "@/lib/menu-items";
+import Image from "next/image";
+import Link from "next/link";
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((menuItem) => (
+        <div className="flex flex-col gap-2 px-2" key={menuItem.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {menuItem.title}
+          </span>
+          {menuItem.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Menu;
